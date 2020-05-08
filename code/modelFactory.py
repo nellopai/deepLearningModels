9c4e282bbc1d38d907d9e045f26c94d0338cca09
aniello <aniello@anyline.com>
2020-05-08 20:28:01 +0200

    adding ResNet18

--- code/modelFactory.py
+++ code/modelFactory.py
@@ -1,5 +1,6 @@
 from networks.vgg16Avg import VGG16GlobalAverage
 from networks.vgg16Depthwise import VGG16DwGlobalAverage
+from networks.resNet18 import ResNet18
 
 
 class ModelFactory(object):
@@ -10,5 +11,7 @@ class ModelFactory(object):
             return VGG16GlobalAverage()
         if type == "vgg16Depthwise":
             return VGG16DwGlobalAverage()
+        if type == "resNet18":
+            return ResNet18()
         assert 0, "Bad model type creation: " + type
     factory = staticmethod(factory)
---
+++ code/networks/resNet18.py
@@ -0,0 +1,52 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Conv2D, MaxPool2D, Input, GlobalAveragePooling2D, Add, Dense, BatchNormalization, Activation
+from tensorflow.keras.regularizers import l2
+from tensorflow.keras.models import Model
+
+
+class ResNet18:
+    def res_net_block(self, input_data, filters, conv_size, stride):
+
+        if stride == 1:
+            shortcut = input_data
+        else:
+            print("entered here")
+            shortcut = Conv2D(filters, 1, strides=(stride, stride),
+                              padding='same')(input_data)
+
+        x = Conv2D(filters, conv_size, activation='relu',
+                   padding='same', strides=(stride, stride))(input_data)
+        x = BatchNormalization()(x)
+        x = Conv2D(filters, conv_size, activation=None, padding='same')(x)
+        x = BatchNormalization()(x)
+        x = Add()([shortcut, x])
+        x = Activation('relu')(x)
+        return x
+
+    def build(self, input_shape, num_classes=3):
+        filters = [64, 128, 256, 512]
+        activation = 'sigmoid' if num_classes == 1 else 'softmax'
+        image = Input(shape=input_shape, name='INPUT_LAYER')
+
+        conv1 = Conv2D(filters=64, kernel_size=(3, 3), strides=(2, 2),
+                       padding="same", activation="relu")(image)
+
+        max_pool1 = MaxPool2D(pool_size=(3, 3), strides=(2, 2))(conv1)
+
+        res_block1 = self.res_net_block(max_pool1, filters[0], 3, 1)
+        res_block2 = self.res_net_block(res_block1, filters[0], 3, 1)
+
+        res_block3 = self.res_net_block(res_block2, filters[1], 3, 2)
+        res_block4 = self.res_net_block(res_block3, filters[1], 3, 1)
+
+        res_block5 = self.res_net_block(res_block4, filters[2], 3, 2)
+        res_block6 = self.res_net_block(res_block5, filters[2], 3, 1)
+
+        res_block7 = self.res_net_block(res_block6, filters[3], 3, 2)
+        res_block8 = self.res_net_block(res_block7, filters[3], 3, 1)
+
+        global_average = GlobalAveragePooling2D()(res_block8)
+        outputs = Dense(num_classes, activation=activation)(global_average)
+
+        model = Model(inputs=image, outputs=outputs)
+        return model
